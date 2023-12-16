5f86df4aaa860b4b83f4f0a301d69a35e0e4decf
libertyimp <libertyimp@debian.search.charter.com>
2023-12-16 15:44:31 -0600

    added link to X

--- partials/about.php
+++ partials/about.php
@@ -3,6 +3,10 @@
     I plan to use this site to display my projects as I complete them.<br>
     I'm a believer in Monero and Bitcoin.<br>
 </p>
+<a class="nostrLink" href="https://twitter.com/libertyimp">
+    <b>Follow me on X!</b>
+</a>
+<br>
 <a class="nostrLink"
     href="https://snort.social/nprofile1qqst7sd7ejyrree3tkdfn7plz95snxtqt7a4j36zxncfrfskz0w9nygpramhxue69uhkummnw3ezuum9w35xvmmjwpexjanpvdujucm0d5hsz9thwden5te0wfjkccte9ejxzmt4wvhxjme0qyv8wumn8ghj7mn9wfhhxarj9eux6u3wwfhkx6mn9uvt4eus">
     <b>Follow me on Nostr!</b>
