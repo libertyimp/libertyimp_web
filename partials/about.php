b895b0d1184a0ff4289585d886452277f6be014d
libertyimp <libertyimp@debian.search.charter.com>
2023-12-24 18:50:25 -0600

    added Youtube link

--- partials/about.php
+++ partials/about.php
@@ -11,3 +11,7 @@
     href="https://snort.social/nprofile1qqst7sd7ejyrree3tkdfn7plz95snxtqt7a4j36zxncfrfskz0w9nygpramhxue69uhkummnw3ezuum9w35xvmmjwpexjanpvdujucm0d5hsz9thwden5te0wfjkccte9ejxzmt4wvhxjme0qyv8wumn8ghj7mn9wfhhxarj9eux6u3wwfhkx6mn9uvt4eus">
     <b>Follow me on Nostr!</b>
 </a>
+<br>
+<a class="nostrLink" href="https://www.youtube.com/channel/UCDrbV_qRfCTczpDVwx8bPIA">
+    <b>Subscribe on YouTube!</b>
+</a>
