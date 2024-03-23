ef206405a62ea21213f920ea322ec5081b442f20
libertyimp <libertyimp@debian.search.charter.com>
2024-03-23 18:29:59 -0500

    removed nostr

--- partials/about.php
+++ partials/about.php
@@ -1,23 +1,19 @@
 <p class="aboutIndex">
     Anarchy, Free Market, Capitalism<br>
     Privacy is vital to a free society.<br>
-    Support the Monero economy on <a class="nostrLink" href="https://monerica.com"><b>Monerica</b></a>.<br>
+    Support the Monero economy on <a class="ipLink" href="https://monerica.com"><b>Monerica</b></a>.<br>
 
 </p>
-<a class="nostrLink" href="https://twitter.com/libertyimp">
+<a class="ipLink" href="https://twitter.com/libertyimp">
     <b>Follow me on X!</b>
 </a>
+
 <br>
-<a class="nostrLink"
-    href="https://snort.social/nprofile1qqst7sd7ejyrree3tkdfn7plz95snxtqt7a4j36zxncfrfskz0w9nygpramhxue69uhkummnw3ezuum9w35xvmmjwpexjanpvdujucm0d5hsz9thwden5te0wfjkccte9ejxzmt4wvhxjme0qyv8wumn8ghj7mn9wfhhxarj9eux6u3wwfhkx6mn9uvt4eus">
-    <b>Follow me on Nostr!</b>
-</a>
-<br>
-<a class="nostrLink" href="https://suchwow.xyz/u/libertyimp">
+<a class="ipLink" href="https://suchwow.xyz/u/libertyimp">
     <b>My Wownero Memes</b>
 
 </a>
 <br>
-<a class="nostrLink" href="https://www.youtube.com/channel/UCDrbV_qRfCTczpDVwx8bPIA">
+<a class="ipLink" href="https://www.youtube.com/channel/UCDrbV_qRfCTczpDVwx8bPIA">
     <b>Subscribe on YouTube!</b>
 </a>
