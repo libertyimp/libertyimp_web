a8d4b7db319e698f67c60a6fd75d6433de2530f5
libertyimp <libertyimp@debian.search.charter.com>
2024-04-16 10:07:21 -0500

    nostr added

--- partials/about.php
+++ partials/about.php
@@ -4,6 +4,12 @@
     Support the Monero economy on <a class="ipLink" href="https://monerica.com"><b>Monerica</b></a>.<br>
 
 </p>
+
+<a class="ipLink" href="https://snort.social/nprofile1qqst7sd7ejyrree3tkdfn7plz95snxtqt7a4j36zxncfrfskz0w9nyg894jma">
+    <b>Follow me on Nostr!</b>
+</a>
+<br>
+
 <a class="ipLink" href="https://twitter.com/libertyimp">
     <b>Follow me on X!</b>
 </a>
