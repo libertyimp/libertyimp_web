18f31cd9e582a0d7518dce5c11c00162482bc923
libertyimp <libertyimp@debian.search.charter.com>
2024-04-24 12:50:20 -0500

    update

--- partials/about.php
+++ partials/about.php
@@ -4,7 +4,7 @@
     Support the Monero economy on <a class="ipLink" href="https://monerica.com"><b>Monerica</b></a>.<br>
 </p>
 
-<a class="ipLink" href="https://nostrudel.ninja/#/u/npub1haqmanygx8nnzhv6n8ur7ytfpxvkqhamt9r5yd8sjxnpvy7utxgs89cvpg">
+<a class="ipLink" href="https://nostr.band/npub1haqmanygx8nnzhv6n8ur7ytfpxvkqhamt9r5yd8sjxnpvy7utxgs89cvpg">
     <b>Follow me on Nostr!</b>
 </a>
 <br>
